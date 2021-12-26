dc398dcc24f656a88bf22e6fa784ef1d46f9576f
hodoabdirizak <hodo.abdirizak@hotmail.com>
2021-12-26 13:06:27 -0500

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,15 @@
 // menu
+const showMenu = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId)
 
+    if(toggle && nav){
+        toggle.addEventListener('click', ()=>{
+            nav.classList.toggle('show')
+        })
+    }
+}
+showMenu('nav-toggle','nav-menu')
 // scroll
 
 // scroll animation
